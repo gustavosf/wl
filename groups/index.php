666c69840a984dd2a80dffe8717b6c4783e59682
Gustavo Seganfredo <gustavosf@gmail.com>
2014-08-07 16:40:50 -0300

    Modificações e configurações efetuadas

---
+++ groups/index.php
@@ -0,0 +1,44 @@
+<?php $exercises = json_decode(file_get_contents(__DIR__.'/exercises.json')); $classe = $_GET['c']; ?>
+<paper-tabs selected="0" style="background-color: #526E9C; color: #fff;">
+	<paper-tab ref="stretches">Alongamentos</paper-tab>
+	<paper-tab ref="exercises">Exercícios</paper-tab>
+	<paper-tab ref="other">Outros</paper-tab>
+</paper-tabs>
+
+<div class="tab-content">
+	<div id="stretches" horizontal layout wrap>
+		<?php foreach ($exercises as $exercise): ?>
+			<?php if (!($exercise->classe == $classe and $exercise->tipo == "A" and $exercise->ativo)) continue; ?>
+			<polymer-ui-card>
+				<h3><?php echo $exercise->nome ?> <small style="font-size:70%">(<?php echo $exercise->regiao ?>)</small></h3>
+				<a href="<?php echo $exercise->link ?>" target="_blank">
+					<img src="groups/img/<?php echo $exercise->thumb ?>" alt=""/>
+				</a>
+			</polymer-ui-card>
+		<?php endforeach ?>
+	</div>
+
+	<div id="exercises" horizontal layout wrap>
+		<?php foreach ($exercises as $exercise): ?>
+			<?php if (!($exercise->classe == $classe and $exercise->tipo == "E" and $exercise->ativo)) continue; ?>
+			<polymer-ui-card>
+				<h3><?php echo $exercise->nome ?> <small style="font-size:70%">(<?php echo $exercise->regiao ?>)</small></h3>
+				<a href="<?php echo $exercise->link ?>" target="_blank">
+					<img src="groups/img/<?php echo $exercise->thumb ?>" alt=""/>
+				</a>
+			</polymer-ui-card>
+		<?php endforeach ?>
+	</div>
+
+	<div id="other" horizontal layout wrap>
+		<?php foreach ($exercises as $exercise): ?>
+			<?php if (!($exercise->classe == $classe and !$exercise->ativo)) continue; ?>
+			<polymer-ui-card>
+				<h3><?php echo $exercise->nome ?> <small style="font-size:70%">(<?php echo $exercise->regiao ?>)</small></h3>
+				<a href="<?php echo $exercise->link ?>" target="_blank">
+					<img src="groups/img/<?php echo $exercise->thumb ?>" alt=""/>
+				</a>
+			</polymer-ui-card>
+		<?php endforeach ?>
+	</div>
+</div>
